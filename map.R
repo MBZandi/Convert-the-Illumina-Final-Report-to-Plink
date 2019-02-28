c8d6ae5797f089e12804e0ed2172e417931808bb
MBZandi <48085022+MBZandi@users.noreply.github.com>
2019-02-28 12:26:46 +0330

    Update map.R

--- map.R
+++ map.R
@@ -8,15 +8,15 @@ setwd(choose.dir())
 getwd()
 
 #2: read the data as follow
- fam <- read.table(file="Sample_Map.txt",  header=F, skip=1)
- head(fam)
+ map <- read.table(file="SNP_Map.txt",  header=F, skip=1)
+ head(map)
 
 # 3: Define the col name of new file
-colnames(fam) <- c("Index",	"Name",	"ID",	"Gender",	"Plate")	
- head(fam)
+colnames(map) <- c("Index",	"SNP-ID",	"Chromosome",	"Position-bp",	"GenTrain-Score",	"SNP",	"ILMN-Strand",	"Customer-Strand",	"Position-CM")
+ head(map)
 
 
-col_order <- c("Name", "Name", "Plate", "Plate", "Plate", "Plate")
+col_order <- c("Chromosome", "SNP-ID", "Position-CM", "Position-bp")
 
 #col_order <- c("ID", "ID", "SNP.Name", "Allele1.F", "Allele2.F")
 #col_order <- c("ID", "ID", "SNP.Name", "Allele1.AB", "Allele2.AB")
@@ -24,12 +24,12 @@ col_order <- c("Name", "Name", "Plate", "Plate", "Plate", "Plate")
 
 
 #4: Define the new datafile with new col order and changeing the cols
- data40x <- fam[, col_order]
+ data40x <- map[, col_order]
  head(data40x)
 
 # 6: export the data to the lgen format for plink as follow:
 
- write.table(data40x, file = "data40-3.fam",sep="\t", row.names=FALSE, col.names=FALSE,  quote = F)
+ write.table(data40x, file = "data40-3.map",sep="\t", row.names=FALSE, col.names=FALSE,  quote = F)
  
 # 7: Or Replace the "-	-" with "0	0" in txtpad sofware 
 ####### Lgen is ready to use
