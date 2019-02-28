44f2ee598399306c5b7a243ce42ae18b43c2095d
MBZandi <48085022+MBZandi@users.noreply.github.com>
2019-02-28 12:25:41 +0330

    Create map.R

---
+++ map.R
@@ -0,0 +1,35 @@
+######## pipeLine for final report to plink format
+
+#remove all stored values
+rm(list=ls(all=TRUE)) 
+
+#change working directory
+setwd(choose.dir())
+getwd()
+
+#2: read the data as follow
+ fam <- read.table(file="Sample_Map.txt",  header=F, skip=1)
+ head(fam)
+
+# 3: Define the col name of new file
+colnames(fam) <- c("Index",	"Name",	"ID",	"Gender",	"Plate")	
+ head(fam)
+
+
+col_order <- c("Name", "Name", "Plate", "Plate", "Plate", "Plate")
+
+#col_order <- c("ID", "ID", "SNP.Name", "Allele1.F", "Allele2.F")
+#col_order <- c("ID", "ID", "SNP.Name", "Allele1.AB", "Allele2.AB")
+
+
+
+#4: Define the new datafile with new col order and changeing the cols
+ data40x <- fam[, col_order]
+ head(data40x)
+
+# 6: export the data to the lgen format for plink as follow:
+
+ write.table(data40x, file = "data40-3.fam",sep="\t", row.names=FALSE, col.names=FALSE,  quote = F)
+ 
+# 7: Or Replace the "-	-" with "0	0" in txtpad sofware 
+####### Lgen is ready to use
